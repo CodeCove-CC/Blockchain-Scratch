09cb98747f1cfe7b668259a9315548b946c43597
CodeCove-CC <cc.codecove@gmail.com>
2023-03-19 12:53:29 +0530

    Part 36

--- app/index.js
+++ app/index.js
@@ -4,6 +4,7 @@ const Blockchain = require('../block_chain/blockchain')
 const P2pServer = require('./p2p-server')
 const Wallet = require('../wallet')
 const TransactionPool = require('../wallet/transaction-pool')
+const Miner = require('./miner')
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001
 
@@ -12,6 +13,7 @@ const bc = new Blockchain()
 const wallet = new Wallet()
 const tp = new TransactionPool()
 const p2pServer = new P2pServer(bc, tp)
+const miner = new Miner(bc, tp, wallet, p2pServer)
 
 app.use(bodyParser.json())
 
@@ -25,6 +27,12 @@ app.post('/mine', (req, res) => {
 
     p2pServer.syncChains()
 
+    res.redirect('/blocks')
+})
+
+app.get('/mine_transactions', (req, res) => {
+    const block = miner.mine()
+    console.log(`New block added: ${block.toString()}`)
     res.redirect('/blocks')
 })
 
